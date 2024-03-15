c6c06f85c9ccd3765074432f97c8b77d096bd7eb
Tomáš Zellerin <tomas@zellerin.cz>
2024-03-15 21:02:53 +0100

    H2load experiments improvements (clip.lisp).
    - add -c parameter (clients)

--- clip.lisp
+++ clip.lisp
@@ -2,13 +2,13 @@
 
 (require 'sb-sprof)
 (defvar *res*)
-(defvar *timed*)
 
 (define-simulator load-system
   :system-name "Load test of TLS server"
   :start-system
   (unless (< iterations multi-threads)
-    (let ((thread (bt:make-thread #'tls-test/async/tls::serve-tls :name "Async TLS server")))
+    (let ((thread (bt:make-thread (lambda () (mini-http2:create-server 8443 tls dispatch-method))
+                                  :name "Async TLS server")))
       (with-output-to-string (*standard-output*)
         (when perf
           (sb-sprof:start-profiling :mode perf :threads (list thread)))
@@ -16,8 +16,10 @@
              (setf *res* (uiop:run-program
                           `("h2load" "https://localhost:8443"
                                      "-n" ,(princ-to-string iterations)
-                                     "-m" ,(princ-to-string multi-threads))
+                                     "-m" ,(princ-to-string multi-threads)
+                                     "-c" ,(princ-to-string clients))
                           :output :string))
+          (bt:interrupt-thread thread #'tls-server:kill-server)
           (bt:join-thread thread)
           (sb-sprof:stop-profiling))))))
 
@@ -48,37 +50,43 @@
 (defclip h2load-full () (:output-file full.txt)
   *res*)
 
-(defclip time-full () (:output-file full.txt)
-  *timed*)
-
-(define-experiment load-simple ()
+(define-experiment load-simple (tls dispatch-method)
   :simulator load-system
-  :instrumentation (h2load-req/s h2load-ttr h2load-suceeded)
-  :variables ((iterations '(10  100 1000 10000))
+  :system-version (format nil "Dispatch: ~a, TLS: ~s" dispatch-method tls)
+  :instrumentation (h2load-finished h2load-req/s h2load-ttr h2load-suceeded)
+  :variables ((iterations '(10000))
               (multi-threads '(1 2 10 20 50))
               (perf '(nil))
-              (sender-fn '(tls-test/async/tls::send-unencrypted-bytes-v1
-                           tls-test/async/tls::send-unencrypted-bytes-v2)))
-  :after-trial (write-current-experiment-data :format :ascii)
-  :before-trial (setf tls-test/async/tls::*unencrypted-sender* sender-fn)
-  :before-experiment (delete-file "/tmp/res.txt")
-  :after-experiment (setf tls-test/async/tls::*unencrypted-sender*
-                          'tls-test/async/tls::send-unencrypted-bytes-v1))
+              (clients '(1 2 5 10)))
+  :after-trial (write-current-experiment-data)
+  :after-experiment (setf tls-server/async/tls::*unencrypted-sender*
+                          'tls-server/async/tls::send-unencrypted-bytes-v1))
 
 (defclip perf-report-full () ()
   (with-output-to-string (*standard-output*)
     (sb-sprof:report)))
 
-(define-experiment perf-test-cpu ()
+(define-experiment perf-test ()
   :simulator load-system
   :instrumentation (perf-report-full h2load-req/s h2load-suceeded h2load-finished)
-  :variables ((iterations '(3000))
+  :variables ((iterations '(300000))
               (multi-threads '(1 10))
               (perf '(:cpu))
-              (sender-fn '(tls-test/async/tls::send-unencrypted-bytes-v1
-                           tls-test/async/tls::send-unencrypted-bytes-v2)))
-  :before-trial (setf tls-test/async/tls::*unencrypted-sender* sender-fn)
+              (clients '(10))
+              (dispatch-method '(:async-custom))
+              (tls '(:tls)))
 
   :after-trial
-  (write-current-experiment-data :format :ascii)
-  :before-experiment (delete-file "/tmp/res-perf.txt"))
+  (write-current-experiment-data :format :ascii))
+
+(define-experiment test-different-implementations ()
+  :simulator load-system
+  :instrumentation (h2load-req/s h2load-suceeded h2load-finished)
+  :variables ((iterations '(3000 10000))
+              (multi-threads '(1 2 5 10 20))
+              (clients '(1 2 5 10))
+              (perf '(:cpu))
+              (tls '(:tls))
+              (dispatch-method '(:none :thread :async-custom)))
+
+  :after-trial (write-current-experiment-data))
