b82a8c8912a8ea999144c5312ba01e6c7e63a2c6
Tomáš Zellerin <tomas@zellerin.cz>
2024-03-21 14:35:20 +0100

    Clips.

--- clip.lisp
+++ clip.lisp
@@ -57,7 +57,7 @@
   :variables ((iterations '(10000))
               (multi-threads '(1 2 10 20 50))
               (perf '(nil))
-              (clients '(1 2 5 10)))
+              (clients '(1 2 5 9)))
   :after-trial (write-current-experiment-data)
   :after-experiment (setf tls-server/async/tls::*unencrypted-sender*
                           'tls-server/async/tls::send-unencrypted-bytes-v1))
@@ -73,6 +73,44 @@
               (multi-threads '(1 10))
               (perf '(:cpu))
               (clients '(10))
+              (dispatch-method '(:async-custom))
+              (tls '(:tls)))
+
+  :after-trial
+  (write-current-experiment-data :format :ascii))
+
+(define-experiment raising-requests ()
+  :simulator load-system
+  :instrumentation (h2load-req/s h2load-suceeded h2load-finished)
+  :variables ((iterations '(10 30 100 300 1000 3000 10000 30000 100000 300000))
+              (multi-threads '(10))
+              (perf '(nil))
+              (clients '(1))
+              (dispatch-method '(:async-custom))
+              (tls '(:tls)))
+
+  :after-trial
+  (write-current-experiment-data))
+
+(defclip number-of-cli (len) ()
+  (length tls-server/async/tls::*clients*))
+
+(defclip nr-of-clients () (:components (number-of-cli)
+                           :trigger-event (tls-server/async/tls::process-client-sockets :BEFORE)
+                           :map-function (list (length tls-server/async/tls::*clients*))))
+
+(define-experiment clients-test ()
+  :simulator load-system
+  :instrumentation (nr-of-clients)
+  :schedule-function
+  (lambda (function time period name &rest options)
+    (declare (ignore name options function time period)))
+  :timestamp get-internal-run-time
+  :seconds-per-time-unit (/ 1.0 internal-time-units-per-second)
+  :variables ((iterations '(30000))
+              (multi-threads '(9))
+              (perf '(nil))
+              (clients '(9))
               (dispatch-method '(:async-custom))
               (tls '(:tls)))
 
