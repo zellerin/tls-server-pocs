31be089ec48b0e2432cadaca67384db9f8fa3190
Tomáš Zellerin <tomas@zellerin.cz>
2024-05-24 14:28:59 +0200

    Clips cleanup - wip

--- clip/setup.lisp
+++ clip/setup.lisp
@@ -1,80 +1,168 @@
 (mgl-pax:define-package #:tls-server/measure
-  (:use :tls-server :clip :cl :mgl-pax))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (require 'sb-sprof))
+  (:use #:tls-server #:clip #:cl #:mgl-pax)
+  (:documentation "Standard clips for TLS servers.")
+  (:import-from #:clip/doc #:clip))
 
 (in-package #:tls-server/measure)
 
+;;;; Use profiling features of sbcl
+#+sbcl
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (require 'sb-sprof))
 
 (defsection tls-server::clips
     (:title "Clips")
-  (actions clip/doc::clip))
+  (@time-clips section)
+  (@perf-clips section))
+
+(defsection @time-clips
+    (:title "Time measuring clips")
+  #+nil  (actions clip/doc::clip)
+  "Time in experiment measure clips.
+
+Sample usage:
+
+```
+(define-simulator dummy  :start-system (sleep 1))
+
+(define-experiment dummy-exp ()
+  :simulator dummy
+  :instrumentation (relative-real-time relative-run-time)
+  :after-trial (write-current-experiment-data))
+```
+
+or
+
+```
+(defclip random-collectable ()
+  (:class clip::composite-time-series-instrumentation
+   :output-file \"ticks\"
+   :components (rnd))
+  (values (random 10)))
+
+(define-experiment dummy-exp-2 ()
+  :simulator dummy
+  :timestamp-clip-name relative-real-time
+  :start-system (dotimes (i 10) (sleep 0.1) (clip::collect 'random-collectable))
+  :instrumentation (random-collectable)
+  :after-trial (write-current-experiment-data))
+```
+
+Note that in second example there is a bug that prevents having
+relative-real-time as an explicit instrumentation (the
+clip::lookup-number-of-samples gives funny results)."
+  (relative-real-time clip)
+  (relative-run-time clip))
 
 (defvar *last-real-time*)
-
 (defclip relative-times ()
-  "Both CPU and real time"
-  (:components (relative-run-time relative-real-time)))
+  "Both CPU and real time since enabling."
+  ())
 
 (defclip relative-real-time ()
-  "Real time since enabling or reset of the clip"
+  "Real time in ms since enabling or reset of the clip"
   (:enable-function (setf *last-real-time* (get-internal-real-time))
    :reset-function  (setf *last-real-time* (get-internal-real-time))
-   :disable-function t
-   :parent relative-times)
-  (- (get-internal-real-time) *last-real-time*))
+   :disable-function t)
+  (round (- (get-internal-real-time) *last-real-time*)
+         (/ internal-time-units-per-second 1000)))
 
 (defvar *last-run-time*)
 (defclip relative-run-time ()
   "Real time since enabling or reset of the clip"
   (:enable-function (setf *last-run-time* (get-internal-run-time))
    :reset-function  (setf *last-run-time* (get-internal-run-time))
-   :disable-function t
-   :parent relative-times)
-  (- (get-internal-run-time) *last-run-time*))
+   :disable-function t)
+  (/ (- (get-internal-run-time) *last-run-time*)
+         (/ internal-time-units-per-second 1000.0)))
+
+(defsection @perf-clips
+    (:title "Performance measure clips")
+  "For SBCL only, there are three sb-sprof based clips. They produce huge amount of
+text to the output and are mutually exclusive."
+  (sb-sprof-alloc clip)
+  (sb-sprof-time clip)
+  (sb-sprof-cpu clip))
 
-;;;; Curl experiment
+(defclip sb-sprof-alloc ()
+  "Performance report as a string."
+  (:enable-function (sb-sprof:start-profiling :mode :alloc
+                                              :threads (list (bt:current-thread)))
+   :disable-function (sb-sprof:stop-profiling)
+   :reset-function (sb-sprof:reset))
+  (prin1-to-string (with-output-to-string (out)
+                     (sb-sprof:report :stream out ))))
 
-(defvar *status* nil)
+(defclip sb-sprof-time ()
+  "Performance report as a string."
+  (:enable-function (sb-sprof:start-profiling :mode :time
+                                              :threads (list (bt:current-thread)))
+   :disable-function (sb-sprof:stop-profiling)
+   :reset-function (sb-sprof:reset))
+  (prin1-to-string (with-output-to-string (out)
+                     (sb-sprof:report :stream out ))))
 
-(defclip curl-res-status ()
-  (:enable-function (setf *status* nil)
-   :disable-function (setf *status* nil)
-   :components (http-output curl-status))
-  (values (prin1-to-string (car *status*)) (third *status*)))
+(defclip sb-sprof-cpu ()
+  "Performance report as a string."
+  (:enable-function (sb-sprof:start-profiling :mode :cpu
+                                              :threads (list (bt:current-thread)))
+   :disable-function (sb-sprof:stop-profiling)
+   :reset-function (sb-sprof:reset))
+  (prin1-to-string (with-output-to-string (out)
+                     (sb-sprof:report :stream out ))))
+
+
+;;;; External process results
+(defvar *external-process-result* nil)
+
+(defun run-process-in-trial (program)
+  (setf *external-process-result*
+        (multiple-value-call 'list (uiop:run-program
+                                    program
+                                    :ignore-error-status t
+                                    :output :string
+                                    :error-output :string))))
+
+(defclip process-result ()
+  "Result and the return value of a process that was run during the trial using
+RUN-PROCESS-IN-TRIAL."
+  (:enable-function (setf *external-process-result* nil)
+   :disable-function (setf *external-process-result* nil)
+   :components (stdout stderr status))
+  (values (prin1-to-string (car *external-process-result*))
+          (prin1-to-string (second *external-process-result*))
+          (third *external-process-result*)))
 
 (defun server-with-external-program (tls model program)
+  "Run HTTP server described by TLS and MODEL. When started, run PROGRAM on it with
+last parameter being the URL of the server."
   (handler-case
       (create-server 0 tls model
                      :announce-url-callback (callback-on-server
                                              (lambda (url)
-                                               (setf *status*
-                                                     (multiple-value-call 'list (uiop:run-program
-                                                                                 `(,@program  ,(puri:render-uri url nil))
-                                                                                 :ignore-error-status t
-                                                                                 :output :string
-                                                                                 :error-output :string))))))
+                                               (run-process-in-trial
+                                                `(,@program  ,(puri:render-uri url nil))))))
     (tls-server::unsupported-server-setup ()
       (restart-case
           (sleep 1)
         (kill-server (res) res))
-      (setf *status* (list "N/A" "Unsupported" -1)))))
+      (setf *external-process-result* (list "N/A" "Unsupported" -1)))))
 
-(define-simulator curl
+(define-simulator program-to-web-server
   :description "Start TLS server on random port (0) and call a curl request to it."
-  :start-system (server-with-external-program tls model
-                                              '("curl" "-k" "--http2-prior-knowledge")))
+  :start-system (server-with-external-program tls model program))
 
 (define-experiment curl ()
   "Run server, run curl request to it."
-  :simulator curl
+  :simulator program-to-web-server
+  :locals ((program '("curl" "-ksS" "--http2-prior-knowledge")))
   :ivs ((model '(:none :thread :async :async-custom))
         (tls '(:tls nil)))
   :timestamp relative-times
-  :instrumentation (relative-times curl-res-status)
+  :instrumentation (relative-real-time relative-run-time process-result sb-sprof-alloc)
   :after-trial (write-current-experiment-data))
 
+
 (defvar *ssl-out* 0)
 
 (defclip ssl-out-write (client vector size res)
@@ -115,7 +203,7 @@
    :components (from to written)
    :output-file "write-octets-to-decrypt")
   (values from to res))
-
+
 (defclip actions (client res)
   "Collect client state and action (data movement) selected for execution into a separate clasp file, actions.clasp."
   (
@@ -131,20 +219,22 @@
                   :key #'length)
           (length  (tls-server/async/tls::client-write-buf client))))
 
+#+nil
 (define-experiment curl-flow ()
   :simulator curl
-  :timestamp get-internal-run-time
+  :timestamp-clip-name get-internal-run-time
   :ivs ((nagle '(nil t)))
   :locals ((model :async-custom) (tls :tls))
   :instrumentation (relative-times actions)
   :before-trial (setf tls-server/async/tls::*nagle* nagle)
   :after-trial (write-current-experiment-data))
 
+#+nil
 (define-experiment h2load-flow ()
   :simulator curl
   :start-system (server-with-external-program tls model
                                               '("h2load" "-n2"))
-  :timestamp get-internal-run-time
+  :timestamp-clip-name get-internal-run-time
   :locals ((nagle t) (model :async-custom) (tls :tls))
   :instrumentation (relative-times actions curl-res-status)
   :before-trial (setf tls-server/async/tls::*nagle* nagle)
@@ -156,87 +246,28 @@
   (run-experiment 'curl :output-file "/tmp/foo.clasp" ))
 
 
-(defvar *res*)
-(defvar *url* "https://localhost:8443")
-
-(defun implementation-port (implementation tls)
-  (unless (and (eq implementation :async-custom)
-               (eq tls nil))
-    (+
-     (ecase implementation
-       (:async-custom 0)
-       (:async 1)
-       (:thread 2)
-       (:none 3))
-     (if tls 8443 8080))))
-
-(defun run-simulation (duration multi-threads clients tls implementation)
-  (setf *res*
-        (if (or (and (> clients 1) (eql implementation :none))
-                (null (implementation-port implementation tls)))
-            ""
-            (uiop:run-program
-             `("h2load" ,(princ-to-string
-                          (tls-server::url-from-port
-                           (implementation-port implementation tls)
-                           "localhost" tls))
-                        "-D" ,(princ-to-string duration)
-                        "-m" ,(princ-to-string multi-threads)
-                        "-c" ,(princ-to-string clients)
-                        "--warm-up-time" "100ms"
-                        "-T" "1"
-                        "-N" "1")
-             :output :string :ignore-error-status t))))
-
-;;;; Simulations
-(define-simulator load-system
-  :system-name "Load test of TLS server"
-  :start-system (run-simulation duration multi-threads clients tls implementation))
-
-
-;;;; Clips for h2load
-
 ;;; Following clips parse specific lines of standard output of h2load
 (defclip h2load-parsed-requests ()
   (:components (total started done succeeded failed errored timeout))
   (multiple-value-bind (match val)
-      (cl-ppcre:scan-to-strings "requests: ([0-9]+) total, ([0-9]+) started, ([0-9]+) done, ([0-9]+) succeeded, ([0-9]+) failed, ([0-9]+) errored, ([0-9]+) timeout" *res*)
+      (cl-ppcre:scan-to-strings "requests: ([0-9]+) total, ([0-9]+) started, ([0-9]+) done, ([0-9]+) succeeded, ([0-9]+) failed, ([0-9]+) errored, ([0-9]+) timeout" *external-process-result*)
     (if match
         (apply 'values (map 'list 'identity val))
         (values 0 0 0 0 0 0 0))))
 
 (defclip h2load-parsed-fin () (:components (finished req/s MB/s))
   (multiple-value-bind (match val)
-      (cl-ppcre:scan-to-strings "finished in ([0-9.]+)s, ([0-9.]+) req/s, (.*)" *res*)
+      (cl-ppcre:scan-to-strings "finished in ([0-9.]+)s, ([0-9.]+) req/s, (.*)" *external-process-result*)
     (if match
         (apply 'values (map 'list 'identity val))
         (values 0 0 0))))
 
 (defclip h2load-parsed-reqtime () (:components (tmin tmax mean sd pm))
   (multiple-value-bind (match val)
-      (cl-ppcre:scan-to-strings "time for request: +([^ ]+) +([^ ]+) +([^ ]+) +([^ ]+) +([^ ]+)%" *res*)
+      (cl-ppcre:scan-to-strings "time for request: +([^ ]+) +([^ ]+) +([^ ]+) +([^ ]+) +([^ ]+)%" *external-process-result*)
     (if match
         (apply 'values (map 'list 'identity val))
         (values 0 0 0 0 0))))
-
-(defclip h2load-full () ()
-  (prin1-to-string *res*))
-
-
-;;;; Clips for perf
-(defclip perf-report ()
-  "Performance report as a string."
-  (:enable-function (sb-sprof:start-profiling :mode :time :threads (list (bt:current-thread)))
-   :disable-function (sb-sprof:stop-profiling)
-   :reset-function (sb-sprof:reset)
-   :components (pr))
-  (prin1-to-string (with-output-to-string (out)
-                     (sb-sprof:report :stream out ))))
-
-(defclip perf-rpw () ()
-  (multiple-value-bind (match val)
-      (cl-ppcre:scan-to-strings " *[0-9]+ +([0-9.]+) [ 0-9.]*foreign function poll" (perf-report))
-    (when match (aref val 0))))
 
 
 ;;;; FDSET clips
@@ -273,10 +304,6 @@
        (error (e) (print e))))
    :name (format nil "Server for ~a" implementation)))
 
-(defun start-threads (tls implementations)
-  (loop for implementation in implementations
-        collect (start-thread tls implementation (implementation-port implementation tls))))
-
 (defun stop-thread (thread)
   (ignore-errors
    ;; thread may already be finished - TODO: handle better
@@ -286,17 +313,10 @@
 (defun stop-threads (threads)
   (mapc #'stop-thread threads))
 
-(define-experiment run-profiled  (duration tls implementation)
-  :system-version (format nil "~a ~a running for ~a secs" tls implementation duration)
-  :simulator load-system
-  :variables ((clients '(1 5)) (multi-threads '(1 10 100)))
-
-  :timestamp get-internal-real-time
-  :schedule-function schedule-in-threads
+(define-experiment run-profiled (duration tls model program clients multi-threads)
+  :system-version (format nil "~a ~a running for ~a secs" tls model duration)
+  :simulator program-to-web-server
+  :timestamp-clip-name relative-run-time
   :deactivate-scheduled-function (lambda (thread) (bt:destroy-thread thread))
-
-  :locals ((thread (start-thread tls implementation)))
-  :instrumentation (h2load-parsed-requests h2load-parsed-reqtime h2load-parsed-fin
-                                           encrypt-buffer-size-all perf-report)
-  :after-trial (write-current-experiment-data)
-  :after-experiment (stop-thread thread))
+  :instrumentation (process-result sb-sprof-cpu)
+  :after-trial (write-current-experiment-data))
