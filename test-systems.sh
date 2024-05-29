82d98c23b7174b85f357746fc7554d650ce10899
Tomáš Zellerin <tomas@zellerin.cz>
2024-05-29 13:53:29 +0200

    Script to test systems.

---
+++ test-systems.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for system in :tls-server/core :tls-server/synchronous :tls-server/async :tls-server/async-openssl :tls-server :tls-server/test :tls-server/clip
+do
+    echo -n "$system: "
+    if sbcl --eval "(ql:quickload $system)" --disable-debugger --quit > $(basename $system).log 2>&1
+    then
+        echo "OK"
+    else
+        echo Failed, see log
+    fi
+done
