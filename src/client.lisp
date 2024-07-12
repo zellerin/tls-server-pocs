6f01d0e84ef6ef3f6583d8b6371e230a8504b931
Tomáš Zellerin <tomas@zellerin.cz>
2024-07-12 17:12:18 +0200

    (fix) UTF8 in client.

--- src/client.lisp
+++ src/client.lisp
@@ -21,53 +21,22 @@
             (http2::process-pending-frames connection nil)))))
     res))
 
-(defclass my-vanilla-client-stream (http2::gzip-decoding-mixin http2::vanilla-client-stream)
-  ((broken-char :accessor get-broken-char :initarg :broken-char :initform nil)))
+
+
+(defclass my-vanilla-client-stream (http2::vanilla-client-stream)
+  ())
 
 (defclass my-client (http2:vanilla-client-connection)
   ()
   (:default-initargs :stream-class 'my-vanilla-client-stream))
 
-(defun utf-is-first-char (octet)
-  (not (= (logand octet #xc0) #x80)))
+(defmethod http2::apply-text-data-frame ((stream my-vanilla-client-stream) payload)
+  (princ payload))
 
-(defun utf-first-char-size (octet)
-  (cond
-    ((= #x00 (logand octet #x80)) 1)
-    ((= #xc0 (logand octet #xe0)) 2)
-    ((= #xe0 (logand octet #xf0)) 3)
-    ((= #xf0 (logand octet #xf8)) 4)
-    ;; 20240607 TODO: Better UTF error
-    (t (error "UTF error"))))
-
-(defmethod http2:apply-data-frame ((stream my-vanilla-client-stream) payload start end)
-  ;; Handle correctly broken (into pieces) multi-octet letters
-  ;;
-  ;; 1) find first start char, and if it is not in the
-  ;; beginning, add all before it to the broken-char
-  ;; this might be slow but rare enough
-  (let* ((first-start-char-position
-           (position-if #'utf-is-first-char payload :start start :end end)))
-    (when (null first-start-char-position)
-      (error "FIXME: no start-char in buffer"))
-    (unless (= first-start-char-position start)
-      ;; FIXME: print broken char
-      (assert (get-broken-char stream))
-      (princ (trivial-utf-8:utf-8-bytes-to-string
-              (concatenate '(vector (unsigned-byte 8)) (get-broken-char stream)
-                           (subseq payload start first-start-char-position))))
-      (setf start first-start-char-position))
-    ;; 2) find whether last starting char is fully in the buffer
-    (let* ((last-start-char-position
-             (position-if #'utf-is-first-char payload :from-end t
-                                                      :start start
-                                                      :end end))
-           (last-start-char (aref payload last-start-char-position)))
-      (unless (= (+ last-start-char-position (utf-first-char-size last-start-char))
-                 end)
-        (setf (get-broken-char stream) (subseq payload last-start-char-position end)
-              end last-start-char-position))
-      (princ (trivial-utf-8:utf-8-bytes-to-string payload :start start :end  end)))))
+(defmethod http2::peer-ends-http-stream ((stream my-vanilla-client-stream))
+  (invoke-restart 'kill-client)
+  ;; 20240609 TODO: send goaway frame first
+  )
 
 (defun dummy-client (url)
   (unless (eql (puri:uri-scheme url) :https)
